c2f6240ef7682feedca8002ecae5f97882231353
Chris Bongers <chrisbongers@gmail.com>
2020-10-25 15:28:41 +0200

    Added list

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  data: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ionViewWillEnter() {
+    this.data = [];
+    this.loadData();
+  }
+
+  loadData(event = null) {
+    this.http
+      .get<{ data: any[] }>(`https://reqres.in/api/unknown`)
+      .subscribe((resp: { data: any }) => {
+          this.data = resp.data;
+      });
+  }
 
 }
