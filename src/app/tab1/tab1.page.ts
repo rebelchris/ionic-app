f1cebc0feb6323770614150343ad77a0cdcca605
Chris Bongers <chrisbongers@gmail.com>
2020-10-26 08:10:20 +0200

    Added infinite loading

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-
   data: any[] = [];
+  page: number = 1;
 
   constructor(private http: HttpClient) {}
 
@@ -18,11 +18,18 @@ export class Tab1Page {
   }
 
   loadData(event = null) {
-    this.http
-      .get<{ data: any[] }>(`https://reqres.in/api/unknown`)
-      .subscribe((resp: { data: any }) => {
-          this.data = resp.data;
-      });
+    setTimeout(() => {
+      this.http
+        .get<{ data: any[] }>(`https://reqres.in/api/unknown?page=${this.page}`)
+        .subscribe((resp: { data: any; total_pages: number }) => {
+          if (this.page !== resp.total_pages) {
+            this.page++;
+          } else {
+            if (event) event.target.disabled = true;
+          }
+          this.data = this.data.concat(resp.data);
+          if (event) event.target.complete();
+        });
+    }, 700);
   }
-
 }
