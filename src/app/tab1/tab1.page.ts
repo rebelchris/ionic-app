d8a6d3e7704026e40b2400946c7929592b9c9032
Chris Bongers <chrisbongers@gmail.com>
2021-03-13 11:01:28 +0100

    Added skeleton loader

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  data = [];
+  constructor() {
+    setTimeout(() => {
+      this.data.push({
+        title: "Test message",
+        description: "Some cool description about this person",
+      });
+    }, 2000);
+  }
 }
